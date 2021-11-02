f5bad42520ad3acf1de477da9c52e55ebc63f0a3
hayzechan <clonghei101899@gmail.com>
2021-11-02 18:58:50 +0800

    [Counter][Hayze] added reducers and fulfilled all requirement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { createStore } from 'redux';
 import './App.css';
+import TodoList from './component/TodoList';
+import { Provider } from 'react-redux';
+import TodoReducer from './component/reducer/TodoReducer';
 
 function App() {
+
+  //installing redux...............
+  const store = createStore(TodoReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <TodoList />
+      </div>
+    </Provider>
   );
 }
 
